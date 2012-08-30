d4b91318f037d9b132741fcc45d3d29c0aef07ee
Jason Leininger <jkleininger@gmail.com>
2012-08-30 16:46:05 -0400

    120830

--- src/net/qrab/broomie/MainActivity.java
+++ src/net/qrab/broomie/MainActivity.java
@@ -5,7 +5,6 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.Rect;
 import android.os.Bundle;
 import android.view.View;
 
@@ -20,17 +19,20 @@ public class MainActivity extends Activity {
     class Panel extends View {
         Bitmap theBitmap;
         String fName;
+        Tilesheet	ts;
 
         public Panel(Context context) {
             super(context);
-            Tilesheet ts = new Tilesheet(context,"dungeon.png",0,0);
+            ts = new Tilesheet(context,"dungeon.png",0,0);
             theBitmap = ts.getBitmap();
         }
  
         @Override
         public void onDraw(Canvas canvas) {
             canvas.drawColor(Color.BLACK);
-            canvas.drawBitmap(theBitmap,new Rect(0,0,60,60),new Rect(0,0,60,60),null);
+            for(int n=0;n<=40;n++){
+            	ts.drawTile(canvas, n, (int)n/10, (int)n%10);
+            }
             //canvas.drawBitmap(theBitmap, 10, 10, null);
         }
 
--- src/net/qrab/broomie/Tilesheet.java
+++ src/net/qrab/broomie/Tilesheet.java
@@ -4,20 +4,24 @@ import java.io.IOException;
 import java.io.InputStream;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Rect;
 import android.content.Context;
-//import android.graphics.Rect;
+import android.graphics.Canvas;
 import android.content.res.AssetManager;
 
 
 public class Tilesheet {
 	AssetManager	aManager;
 	Bitmap			theBitmap;
-	int				rows;
-	int				cols;
-	int				tilewd;
-	int				tileht;
+	int				rows   = 13;
+	int				cols   = 10;
+	int				tileWd = 32;
+	int				tileHt = 32;
 	int				wd;
 	int				ht;
+
+	Rect			rectSrc = new Rect(0,0,tileWd,tileHt);
+	Rect 			rectDst = new Rect(0,0,tileWd,tileHt);
 	
 	public Tilesheet(Context cxt, String fname, int c, int r) {
 		aManager = cxt.getAssets();
@@ -26,6 +30,16 @@ public class Tilesheet {
 
 	public Bitmap getBitmap() {
 		return theBitmap;
+	}
+
+	public void drawTile(Canvas c, int i, int bdCol, int bdRow) {
+		int tsCol = (int)i%cols;
+		int tsRow = (int)i/cols;
+		
+		rectSrc.offsetTo(tsCol*tileWd, tsRow*tileHt);
+		rectDst.offsetTo(bdCol*tileWd, bdRow*tileHt);
+		c.drawBitmap(theBitmap, rectSrc, rectDst, null);
+		System.out.println(i+"->("+tsCol+","+tsRow+")");
 	}
 	
     private Bitmap getBitmapFromAssets(String f) {
